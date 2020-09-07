fcd332eb0a1d8eba6e1a77f66fbcd24ef7c20d79
ZenQien <42192221+ZenQien@users.noreply.github.com>
2020-09-08 07:26:51 +1000

    calc not finished yet

--- Week12/Week12Task.js
+++ Week12/Week12Task.js
@@ -10,4 +10,18 @@ var itemList = document.querySelector('#items');
 
 }
 
+// Here's the javascript for the calculator task
 
+var but = document.getElementByClass("butts");
+var num = document.getElementById("num1");
+var num = document.getElementById("num2");
+var total = document.getElementsByTagName("h4")
+
+for (var thisbutt of but){
+    thisbutt.addEventListener('click', function(){
+        if(this.innerHTML == "=")
+        total.value = eval(total.value);
+        else 
+        calc.value += this.innerHTML;
+    })
+}

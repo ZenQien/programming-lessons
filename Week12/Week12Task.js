94ab7093bdfe1e0171041e3cecf5dcc9db64cecb
ZenQien <42192221+ZenQien@users.noreply.github.com>
2020-09-09 07:16:24 +1000

    calculator still not finished yet
    having trouble with the buttonclick js execution

--- Week12/Week12Task.js
+++ Week12/Week12Task.js
@@ -12,16 +12,24 @@ var itemList = document.querySelector('#items');
 
 // Here's the javascript for the calculator task
 
-var but = document.getElementByClass("butts");
-var num = document.getElementById("num1");
-var num = document.getElementById("num2");
+var plus = document.getElementById('plus').addEventListener
+('click', buttonClick);
+var minus = document.getElementById('minus').addEventListener
+('click', buttonClick);
+var times = document.getElementById('times').addEventListener
+('click', buttonClick);
+var divide = document.getElementById('divide').addEventListener
+('click', buttonClick);
+
+// var but = document.getElementByClass("butts");
+var num1 = document.getElementById("num1");
+var num2 = document.getElementById("num2");
 var total = document.getElementsByTagName("h4")
 
-for (var thisbutt of but){
-    thisbutt.addEventListener('click', function(){
-        if(this.innerHTML == "=")
-        total.value = eval(total.value);
-        else 
-        calc.value += this.innerHTML;
-    })
+function buttonClick(){
+    console.log(total);
+    if(document.getElementById('plus')){
+        total = eval(num1+num2);
+    }
+
 }
